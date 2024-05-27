9a825a300c216afce58b9a1ac8d5ac38e7a06fb0
Mohammad Abubackar <92208635+Abbushaik@users.noreply.github.com>
2024-05-27 12:36:22 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+/*Hamburger Code Starts*/
+const openMenu = document.querySelector("#menu");
+const navbar = document.querySelector(".navbar");
+const closeMenu =document.querySelector("#closeMenu");
+openMenu.addEventListener("click",()=>
+{
+    navbar.classList.toggle("nav-toggle");
+    openMenu.style.display="none";
+    closeMenu.style.display="block";
+});
+closeMenu.addEventListener("click",()=>
+{
+    navbar.classList.toggle("nav-toggle");
+    openMenu.style.display="block";
+    closeMenu.style.display="none";
+});
+
+/*Hamburger Code Ends*/
+
+/*Typing Text Code Starts*/
+var typed = new Typed(".typing-text", {
+    strings: ["Competitive Programming", "Full Stack Web Development"],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 500,
+  });
+/*Typing Text Code Ends*/
+
+/*Sending Email*/
+var submit = document.querySelector(".contact .button-area button");
+submit.addEventListener("click",(event)=>
+{
+    event.preventDefault();
+    var params={
+        from_name:document.getElementById("fullName").value,
+        email_id:document.getElementById("email_id").value,
+        message:document.getElementById("message").value,
+    }
+    emailjs.send("service_ttdipjf","template_4hnur9v",params)
+    .then(function (res)
+    {        
+        alert("Success! "+res.status);
+    });
+});
+
